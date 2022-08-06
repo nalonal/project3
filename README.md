b9405eed139693621086d187495fe3483ce6be48
Leonardional <leonardional@gmail.com>
2022-08-07 02:32:02 +0700

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+git clone https://github.com/nalonal/project3
+cd project3
+chmod u+x starting.sh
--- starting.sh
+++ starting.sh
@@ -4,6 +4,9 @@ sudo apt install lsb-release ca-certificates apt-transport-https software-proper
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
+sudo apt install docker-ce
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 sudo chmod 666 /var/run/docker.sock
-sudo apt install docker-ce
+docker compose up -d
